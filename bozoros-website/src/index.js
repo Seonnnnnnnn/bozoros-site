6f9fd6a45651656f027d316c246afa0e8867ae32
flabiaseona <blankhi60@gmail.com>
2025-04-19 15:26:46 +0700

    Add files via upload

---
+++ bozoros-website/src/index.js
@@ -0,0 +1,7 @@
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+import BozorosLanding from "./BozorosLanding";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<BozorosLanding />);

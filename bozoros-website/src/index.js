7ab005d1468e7959b82b39b044120cc4ef0ac1db
flabiaseona <blankhi60@gmail.com>
2025-04-21 09:40:12 +0700

    Delete bozoros-website/src directory

--- bozoros-website/src/index.js
+++
@@ -1,7 +0,0 @@
-
-import React from "react";
-import { createRoot } from "react-dom/client";
-import BozorosLanding from "./BozorosLanding";
-
-const root = createRoot(document.getElementById("root"));
-root.render(<BozorosLanding />);
